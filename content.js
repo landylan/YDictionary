ff45e6cba119ee3cbacdf462a6e41fd3996d1ec9
ArvinH <arvin0731@gmail.com>
2013-07-21 10:32:16 +0800

    add ui dialog

--- content.js
+++ content.js
@@ -3,25 +3,37 @@
 **/
 function paresWeb(query, event){
     var TD = $.get("http://tw.dictionary.search.yahoo.com/search?q="+query, function(data) {
-            var el = $( '<div></div>' );
-            el.html(data);
-            var attrs = $('p[class=explanation]', el);
-            var rangeCount = window.getSelection().rangeCount;
-            var range = window.getSelection().getRangeAt(rangeCount-1);
-            var dummy = document.createElement("div");
-            dummy.className = "result_content";
-            range.insertNode(dummy);
-            $(".result_content").css({"width":"40px", "height":"20px", "display":"inline"})
-            $(".result_content").text(attrs.eq(0).html())
-            /*var box = document.getBoxObjectFor(dummy);
-            var x = box.x, y = box.y;
-            dummy.parentNode.removeChild(dummy);
-            */
-                $(".result_content").tooltip({content: attrs.eq(0).html()});
-                setTimeout(function(){
-                    dummy.parentNode.removeChild(dummy);
-                },3000);
-            })
+                var el = $( '<div></div>' );
+                el.html(data);
+                var attrs = $('p[class=explanation]', el);
+                var rangeCount = window.getSelection().rangeCount;
+                var range = window.getSelection().getRangeAt(rangeCount-1);
+                var dummy = document.createElement("div");
+                dummy.setAttribute("id","dialog");
+                dummy.setAttribute("title","translated result");
+                dummy.className = "result_content";
+                range.insertNode(dummy);
+                $(".result_content").css({"width":"40px", "height":"20px", "display":"inline", "z-index":"3000"})
+                $(".result_content").text(attrs.eq(0).html());
+                var box = dummy.getBoundingClientRect();
+                var x = box.left+25, y = box.top+15;
+                console.log(x)
+                
+                jQuery("#dialog").dialog({
+                  autoOpen: true, 
+                  draggable: true,
+                  resizable: true,
+                  height: 'auto',
+                  width: "250px",
+                  modal: true,
+                  open: function(event, ui) {
+                    $(event.target).parent().css('position','fixed');
+                    $(event.target).parent().css('top', y+'px');
+                    $(event.target).parent().css('left', x+'px');
+                  }
+                });
+                   
+          })
     .fail(function() { alert("error"); });
 }
  
