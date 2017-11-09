12e0fb8ba1b83cbde95c67ce4068181b10d56462
landylan <landylan.163@gmail.com>
2017-11-09 20:01:49 +0800

    改成可用，順便換一換 jquery

--- content.js
+++ content.js
@@ -2,37 +2,56 @@
   @author ArvinH
 **/
 function paresWeb(query, event){
-    var TD = $.get("http://tw.dictionary.search.yahoo.com/search?q="+query, function(data) {
+    var TD = $.get("https://tw.dictionary.search.yahoo.com/search?q="+query, function(data) {
                 var el = $( '<div></div>' );
                 el.html(data);
-                var attrs = $('p[class=explanation]', el);
+                // var attrs = $('p[class=explanation]', el);
+                var attrs = $('p[class=DictionaryResults]', el);
+                // exp = el.find('div[class=DictionaryResults][class=1st][class=explain]');
+                exp = el.find('.DictionaryResults');
+                
                 var rangeCount = window.getSelection().rangeCount;
                 var range = window.getSelection().getRangeAt(rangeCount-1);
+                /*
+                if (window.getSelection) {
+                    rangeCount = window.getSelection().rangeCount;
+                    range = window.getSelection().getRangeAt(rangeCount-1);
+                } else if (document.getSelection) {
+                    rangeCount = document.getSelection().rangeCount;
+                    range = document.getSelection().getRangeAt(rangeCount-1);
+                } else if (document.selection) {
+                    rangeCount = document.selection.rangeCount;
+                    range = document.selection.getRangeAt(rangeCount-1);
+                } 
+                */
                 var dummy = document.createElement("div");
                 dummy.setAttribute("id","dialog");
-                dummy.setAttribute("title","translated result");
+                dummy.setAttribute("title","翻譯結果");
                 dummy.className = "result_content";
                 range.insertNode(dummy);
+                
                 $(".result_content").css({"width":"40px", "height":"20px", "display":"inline", "z-index":"3000"})
-                $(".result_content").text(attrs.eq(0).html());
                 var box = dummy.getBoundingClientRect();
                 var x = box.left+25, y = box.top+15;
-                console.log(x)
+                console.log(x);
+                console.log(y);
+
+                $(".result_content").html(exp.eq(2).html());
                 
                 jQuery("#dialog").dialog({
                   autoOpen: true, 
                   draggable: true,
                   resizable: true,
                   height: 'auto',
-                  width: "250px",
-                  modal: true,
+                  width: 'auto', // "250px",
+                  modal: false, //true,
                   open: function(event, ui) {
                     $(event.target).parent().css('position','fixed');
                     $(event.target).parent().css('top', y+'px');
                     $(event.target).parent().css('left', x+'px');
                   }
                 });
-                   
+                //*/   
           })
     .fail(function() { alert("error"); });
 }
